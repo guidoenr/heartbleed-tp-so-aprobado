00f4f56a0137a604e0d9a7df93c83fcbcd5c6f1b
utnso <martirolon.mr@gmail.com>
2020-04-23 22:19:41 -0300

    Team: agrego cambios a los objetivos

--- team/src/team.h
+++ team/src/team.h
@@ -7,34 +7,26 @@
 #include "/home/utnso/workspace/tp-2020-1c-heartbleed/Utils/src/Utils.h"
 #include "/home/utnso/workspace/tp-2020-1c-heartbleed/Utils/src/Utils.c"
 
+
 typedef struct {
 	char* pokemon;
-	int* prev;
 	int* next;
 } t_lista_pokemons;
 
 typedef struct { // capaz tengamos que agregar un id
     int posicion[2];
-    int* prev;
 	int* next;
 } t_lista_posiciones;
 
 typedef struct {
-	t_lista_pokemons* pokemon;
-	int* prev;
+	t_lista_pokemons* pokemons;
 	int* next;
-} t_lista_pokemon_entrenadores;
-
-typedef struct {
-	t_lista_pokemons* pokemon;
-	int* prev;
-	int* next;
-} t_lista_objetivos_entrenadores;
+} t_lista_lista_pokemons;
 
 typedef struct {
 	t_lista_posiciones* posiciones_entrenadores;
-	t_lista_pokemon_entrenadores* pokemon_entrenadores;
-	t_lista_objetivos_entrenadores* objetivos_entrenadores;
+	t_lista_lista_pokemons* pokemon_entrenadores;
+	t_lista_lista_pokemons* objetivos_entrenadores;
     int tiempo_reconexion;
 	int retardo_cpu;
 	char* algoritmo_planificacion;
@@ -46,10 +38,12 @@ typedef struct {
 
 t_config_team* config;
 t_log* logger;
+t_lista_pokemons* obtener_objetivo_global();
 void iniciar_logger(void);
 void leer_config(void);
 void terminar_programa(int, t_log*, t_config_team*);
 void liberar_conexion(int);
 void liberar_logger(t_log* logger);
 void liberar_config(t_config_team*);
+
 
