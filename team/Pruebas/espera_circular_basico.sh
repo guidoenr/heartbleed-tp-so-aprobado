9e34a2817db0f5cf053264f5d8f2c60c3bf3453a
utnso <martirolon.mr@gmail.com>
2020-07-17 19:45:42 -0300

    Pruebas team

---
+++ team/Pruebas/espera_circular_basico.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+./game_boy TEAM APPEARED_POKEMON Jolteon 2 2
+./game_boy TEAM APPEARED_POKEMON Flareon 4 6
+./game_boy TEAM APPEARED_POKEMON Umbreon 10 6
+./game_boy TEAM APPEARED_POKEMON Espeon 7 1
+./game_boy TEAM APPEARED_POKEMON Vaporeon 4 10
---
+++ team/Pruebas/prueba_final_team.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+./game_boy TEAM APPEARED_POKEMON Pikachu 9 9
+./game_boy TEAM APPEARED_POKEMON Squirtle 6 7
+./game_boy TEAM APPEARED_POKEMON Onix 2 8
+./game_boy TEAM APPEARED_POKEMON Gengar 6 6
+./game_boy TEAM APPEARED_POKEMON Squirtle 5 5
