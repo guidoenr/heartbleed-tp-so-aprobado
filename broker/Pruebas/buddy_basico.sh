a56aa8bf4c9bad80b0964a3fd5ae2339dcbef6f3
utnso <martirolon.mr@gmail.com>
2020-07-17 22:45:28 -0300

    Pruebas broker

---
+++ broker/Pruebas/buddy_basico.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd /home/utnso/workspace/tp-2020-1c-heartbleed/game_boy/Debug/
+./gameboy BROKER CAUGHT_POKEMON 1 OK
+./gameboy BROKER CAUGHT_POKEMON 2 FAIL
+
+./gameboy BROKER NEW_POKEMON Pikachu 2 3 1
+
+./gameboy BROKER CATCH_POKEMON Onyx 4 5
+
+./gameboy SUSCRIPTOR NEW_POKEMON 10
+
+./gameboy BROKER CATCH_POKEMON Charmander 4 5
---
+++ broker/Pruebas/cargar_memoria_simple.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd /home/utnso/workspace/tp-2020-1c-heartbleed/game_boy/Debug/
+./gameboy BROKER GET_POKEMON Pikachu
+./gameboy BROKER GET_POKEMON Charmander
